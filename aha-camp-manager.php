8c7911eb5939cd42a845cb9bb9a62fa3a4cff5ce
Noga Vigdor <noga.vigdor@gmail.com>
2023-04-28 22:33:50 +0300

    Solved settings bug
    Now the sections appear in the settings

--- aha-camp-manager.php
+++ aha-camp-manager.php
@@ -27,31 +27,41 @@
 
 class AhaCampManager {
     function __construct(){
+       
         add_action('admin_menu', array($this, 'admin_page'));
         add_action('admin_init', array($this, 'settings'));
+    
     }
 
 function settings(){
-
+   
     add_settings_section('aha_form_first_section', 'Form', array($this, 'parent_details'), 'aha-settings');
     add_settings_field('aha_form_parentFirstName', 'First Name', array($this, 'parentFirstNameHTML'),'aha-settings', 'aha_form_first_section' );
-    register_setting('ahaplugin','aha_form_parentFirstName', array('sanitize_callback'=>'sanitize_text_field', 'default'=>''));
+    register_setting('ahaplugin','aha_form_parentFirstName', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0', 'section'=>'aha_form_first_section'));
+    add_settings_field('aha_form_parentLastName', 'Last Name', array($this, 'parentLastNameHTML'),'aha-settings', 'aha_form_first_section' );
+    register_setting('ahaplugin','aha_form_parentLasttName', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0', 'section'=>'aha_form_first_section'));
 
   
 }
-
 
 function parent_details() {
     echo 'This is the parent details section.';
 }
 
 function parentFirstNameHTML() {
-    var_dump('parentFirstNameHTML is called');
-    die();
+   
 ?>
  <input name='aha_form_parentFirstName' type="text">
 <?php
 }
+
+function parentLastNameHTML() {
+   
+    ?>
+     <input name='aha_form_parentLastName' type="text">
+    <?php
+    }
+    
 
     //creating the dashboard settings funcgtionality
 function admin_page(){
@@ -65,8 +75,8 @@ function admin_page(){
 }
 
 
-
 //The content of the plugin page once the Aha Settings is being clicked at the dashboard settings menu
+
 function settingsHTML() { ?>
     <div class="wrap">
         <h1>Aha Camp Manager Settings</h1>
@@ -74,7 +84,7 @@ function settingsHTML() { ?>
         <form action="options.php" method="POST">
             <?php
                 settings_fields('ahaplugin');
-                do_settings_sections('aha_form_first_section');
+                do_settings_sections('aha-settings');
                 submit_button();
             ?>
         </form>
@@ -82,6 +92,8 @@ function settingsHTML() { ?>
    <?php } 
 
 }
+
+
 
 $ahaCampManager=new AhaCampManager();
 
