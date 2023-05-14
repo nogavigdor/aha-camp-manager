2cc74bbbd6a156ff253078d5b79bf158804017cd
Noga Vigdor <noga.vigdor@gmail.com>
2023-05-15 00:50:59 +0300

    Text Domain and Setting
    Added text domain (translation) functionality and updated plugins settings location

--- aha-camp-manager.php
+++ aha-camp-manager.php
@@ -10,29 +10,23 @@
  * License: GPLv2 or later
  *
  * Copyright 2023 noga.digital
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
  */
 
 class AhaCampManager {
     function __construct(){
-       
+        add_action('plugins_loaded', array($this, 'load_text_domain'));
         add_action('admin_menu', array($this, 'admin_page'));
         add_action('admin_init', array($this, 'settings'));
-    
+        
     }
 
+//loads the plugin's translation
+function load_text_domain() {
+        load_plugin_textdomain( 'aha-camp-manager', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+ }
+       
+//Setting up the plugin's page, sections and fields
 function settings(){
    
     add_settings_section('aha_form_first_section', 'Form', array($this, 'parent_details'), 'aha-settings');
@@ -63,7 +57,7 @@ function parentLastNameHTML() {
     }
     
 
-    //creating the dashboard settings funcgtionality
+//creating the dashboard settings funcgtionality and out
 function admin_page(){
     //first parameter: the title of the page that will appear on the tab,
     //second parameter: the title of the page that will be used in the dashboard setting menu
@@ -71,9 +65,9 @@ function admin_page(){
     //forth parameter: the name of the slug
     //fifth parameter: the function that will create the content for these settings page
     //Sixth parameter: the item position in the menu - in this case first position
-    add_options_page('Aha Camp Manager', 'AHA Settings', 'manage_options', 'aha-settings', array($this, 'settingsHTML'), '0');
+    add_menu_page('Aha Camp Manager', 'AHA Manager  ', 'manage_options', 'aha-settings', array($this, 'settingsHTML'), plugins_url( 'aha-camp-manager/img/soccer-player.png' ),
+    2);
 }
-
 
 //The content of the plugin page once the Aha Settings is being clicked at the dashboard settings menu
 
